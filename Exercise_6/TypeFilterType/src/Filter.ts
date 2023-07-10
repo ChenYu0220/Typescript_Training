bb4d42be73544fd271dc3d0dcb18f325be33b78f
chenyu <cy1273971423@gmail.com>
2023-07-10 16:24:06 +0800

    feat: 新增过滤器的两种方法

--- Exercise_6/TypeFilterType/src/Filter.ts
+++ Exercise_6/TypeFilterType/src/Filter.ts
@@ -1,4 +1,7 @@
-// 遍历 Object 和 递归方式两种
+// 遍历 Object
 export type KFilter<T,U> = {
-    
-};
+    [key in keyof T] : T[key] extends U ? key : never;
+}[keyof T];
+
+// 递归方式
+export type SFilter<T,U> = T extends [] ? [] : T extends [infer H, ...infer R] ? [H, ...SFilter<R, U>] : [];
